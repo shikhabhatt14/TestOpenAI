7a231797ee1c494ee97e829c48f69abec36e7ebd
shikhabhatt14 <shikhabhatt14@gmail.com>
2025-02-25 12:34:04 +0530

    Create TestOpenAiApplicationTests.java

---
+++ src/test/java/com/trial/openai/testOpenAI/TestOpenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trial.openai.testOpenAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOpenAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
