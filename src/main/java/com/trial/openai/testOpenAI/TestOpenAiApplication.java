b960707a9f299738e0bc7bd6d804fc3e29afad33
shikhabhatt14 <shikhabhatt14@gmail.com>
2025-02-25 12:30:17 +0530

    Create TestOpenAiApplication.java

---
+++ src/main/java/com/trial/openai/testOpenAI/TestOpenAiApplication.java
@@ -0,0 +1,13 @@
+package com.trial.openai.testOpenAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOpenAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOpenAiApplication.class, args);
+	}
+
+}
